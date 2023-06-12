c72638abda071201d1db99b074b37f3f8c6ea0b7
rkfcl <kwmkr6@gmail.com>
2023-06-13 02:11:29 +0900

    npc 명령어 수정

--- src/main/java/com/rkfcl/server_info/commands/NPCShopCommand.java
+++ src/main/java/com/rkfcl/server_info/commands/NPCShopCommand.java
@@ -4,10 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -15,16 +12,33 @@ import org.bukkit.entity.Villager;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.rkfcl.server_info.Manager.ItemManager.createOreItem;
 import static com.rkfcl.server_info.Manager.ItemManager.namechange;
 
-public class NPCShopCommand implements CommandExecutor {
+public class NPCShopCommand implements CommandExecutor, TabCompleter {
 
     private JavaPlugin plugin;
-
+    private List<String> professionList;
     public NPCShopCommand(JavaPlugin plugin) {
         this.plugin = plugin;
+        this.professionList = new ArrayList<>();
+        professionList.add("ARMORER");
+        professionList.add("BUTCHER");
+        professionList.add("CARTOGRAPHER");
+        professionList.add("CLERIC");
+        professionList.add("FARMER");
+        professionList.add("FISHERMAN");
+        professionList.add("FLETCHER");
+        professionList.add("LEATHERWORKER");
+        professionList.add("LIBRARIAN");
+        professionList.add("MASON");
+        professionList.add("SHEPHERD");
+        professionList.add("TOOLSMITH");
     }
+
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -32,30 +46,36 @@ public class NPCShopCommand implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "이 명령어는 플레이어만 사용할 수 있습니다.");
             return true;
         }
-
         Player player = (Player) sender;
-
+        Location playerLocation = player.getLocation();
         if (command.getName().equalsIgnoreCase("setnpcshop")) {
-            if (args.length != 4) {
-                player.sendMessage(ChatColor.RED + "잘못된 명령어 형식입니다. 사용법: /setnpcshop [npc 이름] [x] [y] [z]");
+            if (args.length != 2) {
+                player.sendMessage(ChatColor.RED + "잘못된 명령어 형식입니다. 사용법: /setnpcshop [npc 이름] [직업]");
                 return true;
             }
 
             String npcName = args[0];
-            double x, y, z;
+            String professionArg = args[1];
 
-            try {
-                x = Double.parseDouble(args[1]);
-                y = Double.parseDouble(args[2]);
-                z = Double.parseDouble(args[3]);
-            } catch (NumberFormatException e) {
-                player.sendMessage(ChatColor.RED + "좌표는 숫자로 입력해야 합니다.");
+            // 직업 목록을 출력
+            if (professionArg.equalsIgnoreCase("list")) {
+                player.sendMessage(ChatColor.YELLOW + "직업 목록:");
+                for (String profession : professionList) {
+                    player.sendMessage(ChatColor.GRAY + "- " + profession);
+                }
                 return true;
             }
 
-            Location location = new Location(player.getWorld(), x, y, z);
+            Villager.Profession profession;
+            try {
+                profession = Villager.Profession.valueOf(professionArg.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                player.sendMessage(ChatColor.RED + "유효하지 않은 직업입니다.");
+                return true;
+            }
+            Location location = new Location(player.getWorld(), playerLocation.getX(), playerLocation.getY(), playerLocation.getZ());
 
-            spawnNPC(location, npcName);
+            spawnNPC(playerLocation, npcName, profession);
 
             player.sendMessage(ChatColor.GREEN + npcName + " NPC가 " + location.getBlockX() + ", " + location.getBlockY() + ", " + location.getBlockZ() + "에 소환되었습니다.");
 
@@ -97,9 +117,30 @@ public class NPCShopCommand implements CommandExecutor {
         }
         return true;
     }
-    private void spawnNPC(Location location, String npcName) {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (command.getName().equalsIgnoreCase("setnpcshop")) {
+            if (args.length == 2) {
+                String partialArg = args[1].toLowerCase();
+                List<String> matchingProfessions = new ArrayList<>();
+                for (String profession : professionList) {
+                    if (profession.toLowerCase().startsWith(partialArg)) {
+                        matchingProfessions.add(profession);
+                    }
+                }
+                return matchingProfessions;
+            }
+        }
+        return null;
+    }
+    private void spawnNPC(Location location, String npcName, Villager.Profession profession) {
         Villager npc = (Villager) location.getWorld().spawnEntity(location, EntityType.VILLAGER);
+        // 플레이어의 위치와 바라보는 방향으로 NPC를 소환
         npc.setAI(false); // NPC가 움직이지 않도록 설정
+
+        npc.teleport(location.add(location.getDirection()));
+
+        npc.setProfession(profession);
         npc.setCustomName(npcName);
         npc.setCustomNameVisible(true);
         npc.setInvulnerable(true); // NPC를 무적으로 설정
@@ -118,4 +159,6 @@ public class NPCShopCommand implements CommandExecutor {
             }
         }
     }
+
 }
+
